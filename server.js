f9fcf89b81dff115417819eb43fcf057034e0ea5
ivansomertse <ivansomertse@gmail.com>
2025-12-23 08:16:33 +0800

    debug

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(
       "http://localhost:5500",
       "http://127.0.0.1:5500",
       "http://localhost:3000",
-      "https://student-info-system.vercel.app"
+      "https://student-info-systemv20.vercel.app"
     ],
     methods: ["GET", "POST", "DELETE", "OPTIONS"],
   })
